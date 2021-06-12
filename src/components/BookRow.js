1c260b89671d1f0dd5becd8f03338be5415ed0d8
suzan-naeem <64129170+suzan-naeem@users.noreply.github.com>
2021-06-12 03:00:43 +0200

    add link to book details

--- src/components/BookRow.js
+++ src/components/BookRow.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../style/admin.css';
 import '../style/ratingStars.css';
+import { Link } from "react-router-dom";
 
 class BookRow extends Component {
 
@@ -9,10 +10,21 @@ class BookRow extends Component {
 
                 <div className="row ">
 
-                   {this.props.items.map(book=>
-                    <div className="col-4 col-md-2 book" >
+                {this.props.items.map(book=> 
+                   <div className="col-4 col-md-2 book" >
                     <figure>
+                    
+                    < div className="book_img">
                     <img style={{width:'100%'}} src={book&&book.volumeInfo.imageLinks.thumbnail} alt="" className="  book_image rounded  img-fluid"/>
+                       <div className="hoverable">
+                         <Link to={`/BookDetails/${book.volumeInfo.industryIdentifiers[0].identifier}`} style={{ textDecoration: 'none' }}>
+                             <span className="details">details</span>
+                          </Link>
+                         <span className="icon-heart">
+                           <i className="fa fa-heart "></i>
+                         </span>
+                        </div>
+                     </div>  
                     <figcaption className="book_title" style={{alignItems:'center'}}>{book&&book.volumeInfo.title.slice(0,15)}</figcaption>
                         <span className="active-star"></span>
                         <span className="active-star"></span>
