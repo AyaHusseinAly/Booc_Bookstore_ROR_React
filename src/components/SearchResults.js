0c0a8f0ffaf2d9b6758854f551a512c954b4d941
suzan-naeem <64129170+suzan-naeem@users.noreply.github.com>
2021-06-12 05:14:32 +0200

    edit search page

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-// import '../style/admin.css';
+import '../style/search.css';
 import '../style/ratingStars.css';
+import { Link } from "react-router-dom";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import '../style/headerFooter.css';
 
@@ -9,19 +10,30 @@ class SearchResults extends Component {
     render() {
 
         return (
-            <div className="container d-flex flex-column ">
+           
+            <div className="row " style={{padding: '150px'}}>
 
-            {this.props.items.map(book=> <div className="col-4 col-md-4 book" >
-                <figure className="d-flex">
-                        <img style={{width:'100%'}} src={book&&book.volumeInfo.imageLinks&&book.volumeInfo.imageLinks.thumbnail} alt="" className="  book_image rounded  img-fluid"/>
-                        <div>
-                            <figcaption className="" style={{alignItems:'center'}}>{book&&book.volumeInfo.title}</figcaption>
-                            <span className="active-star"></span>
-                            <span className="active-star"></span>
-                            <span className="active-star"></span>
-                            <span className="clip-star"></span>
-                            <span className="clip-star"></span>
-                        </div>
+            {this.props.items.map(book=> 
+               <div className="col-4 col-md-2 book" >
+                <figure>
+                
+                < div className="book_img">
+                <img style={{width:'100%'}} src={book&&book.volumeInfo.imageLinks.thumbnail} alt="" className="  book_image rounded  img-fluid"/>
+                   <div className="hoverable">
+                     <Link to={`/BookDetails/${book&&book.volumeInfo.industryIdentifiers[0].identifier}`} style={{ textDecoration: 'none' }}>
+                         <span className="details">details</span>
+                      </Link>
+                     <span className="icon-heart">
+                       <i className="fa fa-heart "></i>
+                     </span>
+                    </div>
+                 </div>  
+                <figcaption className="book_title" style={{alignItems:'center'}}>{book&&book.volumeInfo.title.slice(0,15)}</figcaption>
+                    <span className="active-star"></span>
+                    <span className="active-star"></span>
+                    <span className="active-star"></span>
+                    <span className="clip-star"></span>
+                    <span className="clip-star"></span>
                 </figure>
                 </div>)}
 
