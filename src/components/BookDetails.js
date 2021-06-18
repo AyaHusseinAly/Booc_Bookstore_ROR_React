77fc4cf8e42747ff42710bdbafc1ff376b12403c
suzan-naeem <64129170+suzan-naeem@users.noreply.github.com>
2021-06-18 21:49:39 +0200

    get rating from api

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,12 +1,32 @@
 import React, {useState,useEffect } from 'react';
+// import { useHistory ,useLocation } from 'react-router-dom';
+
 import '../style/admin.css';
 import '../style/BookDetails.css';
 import { Link } from "react-router-dom";
 import BookRow from './BookRow';
 import axios from 'axios';
+import {
+    EmailIcon,
+    FacebookIcon,
+    LinkedinIcon,   
+    TwitterIcon,
+    WhatsappIcon,   
+  } from "react-share";
+import {
+    EmailShareButton,
+    FacebookShareButton,
+    LinkedinShareButton,
+    TwitterShareButton,
+    WhatsappShareButton,
+    
+  } from "react-share";
+
 
 const BookDetails = (props) => {
     const [book, setBook] = useState([]);
+    // const location = useLocation();
+    // const history = useHistory();
 
     useEffect(() =>{
         const isbn = props.match.params.isbn;
@@ -22,6 +42,8 @@ const BookDetails = (props) => {
     }, [])
         
         console.log(book[0]) 
+        // console.log(history.location.pathname)
+        //  console.log(window.location.href)
         return(
             
             <> 
@@ -35,10 +57,11 @@ const BookDetails = (props) => {
                                 <img src={book.length > 0 && book[0].volumeInfo.imageLinks.thumbnail} style={{ width: '400px'}}/>
                                 <div className="button">
                                    
-//                                      <button><i className="fa fa-download "></i> Download</button>
-                                    <button><i className="fa fa-book " ></i> 
+                                     <button><i className="fa fa-book " ></i> 
                                      <a href={book.length > 0 && book[0].volumeInfo.previewLink}  >Preview</a> </button>
                                     <button><i className="fa fa-microphone"></i> Find near me</button>
+                                    {book.length > 0 && book[0].accessInfo.pdf.isAvailable ? 
+                                    <button><i className="fa fa-download"></i> <a href={book.length > 0 && book[0].accessInfo.pdf.acsTokenLink}>Download as PDF</a></button>:<span>  </span>}
                                   
                                 </div>
                             </div>
@@ -46,30 +69,94 @@ const BookDetails = (props) => {
                     </div>
                     <div className="col-xs-12 col-sm-12 col-md-7 col-lg-6">
                         <div className="box-info">
-                            <span>Free</span>
+                        {book.length > 0 && book[0].accessInfo.pdf.isAvailable ? 
+                            <span>Free</span>:<span>Not Free</span>}
                             <div className="heading">
                                 <h2>{book.length > 0 && book[0].volumeInfo.title}</h2>
                                 <h5>from {book.length > 0 && book[0].volumeInfo.categories} section</h5>
 
-                                {book.length > 0 && book[0].volumeInfo.averageRating ? 
+                                 {book.length > 0 && book[0].volumeInfo.averageRating ?
+                                
+                                
+                                    ((Math.round(book[0].volumeInfo.averageRating) === 1 &&
                                 <div className="evaluation">
-                                <i className="fa fa-star" style={{color: 'orange'}}></i>
-                                <i className="fa fa-star" style={{color: 'orange'}} ></i>
+                                    Rating  : 
+                                <i className="fa fa-star" style={{color: 'orange',marginLeft: '10px'}}></i>
+                                <i className="fa fa-star" ></i>
                                 <i className="fa fa-star"></i>
                                 <i className="fa fa-star"></i>
                                 <i className="fa fa-star"></i>
-
-                                    <span>({book[0].volumeInfo.averageRating} Rating )</span> 
+                                    <span>({Math.round(book[0].volumeInfo.averageRating)} /5)</span> 
                                      
-                                </div> :
-                                <span> No rating yet </span>
+                                </div>) || 
+                                (Math.round(book[0].volumeInfo.averageRating) === 2 &&
+                                    <div className="evaluation">
+                                        Rating  : 
+                                    <i className="fa fa-star" style={{color: 'orange',marginLeft: '10px'}}></i>
+                                    <i className="fa fa-star" style={{color: 'orange'}} ></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                        <span>({Math.round(book[0].volumeInfo.averageRating)} /5)</span> 
+                                         
+                                </div>) || 
+                                (Math.round(book[0].volumeInfo.averageRating) === 3 &&
+                                    <div className="evaluation">
+                                        Rating  : 
+                                    <i className="fa fa-star" style={{color: 'orange',marginLeft: '10px'}}></i>
+                                    <i className="fa fa-star" style={{color: 'orange'}} ></i>
+                                    <i className="fa fa-star" style={{color: 'orange'}}></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                        <span>({Math.round(book[0].volumeInfo.averageRating)} /5)</span> 
+                                         
+                                 </div>) || 
+                                 (Math.round(book[0].volumeInfo.averageRating) === 4 &&
+                                    <div className="evaluation">
+                                      Rating  : 
+                                    <i className="fa fa-star" style={{color: 'orange',marginLeft: '10px'}}></i>
+                                    <i className="fa fa-star" style={{color: 'orange'}} ></i>
+                                    <i className="fa fa-star" style={{color: 'orange'}}></i>
+                                    <i className="fa fa-star" style={{color: 'orange'}}></i>
+                                    <i className="fa fa-star"></i>
+                                        <span>({Math.round(book[0].volumeInfo.averageRating)} /5)</span> 
+                                         
+                                </div>) || 
+                                (book[0].volumeInfo.averageRating === 4.5 &&
+                                <div className="evaluation">
+                                  Rating  : 
+                                <i className="fa fa-star" style={{color: 'orange',marginLeft: '10px'}}></i>
+                                <i className="fa fa-star" style={{color: 'orange'}} ></i>
+                                <i className="fa fa-star" style={{color: 'orange'}}></i>
+                                <i className="fa fa-star" style={{color: 'orange'}}></i>
+                                <i class="fa fa-star-half-o " style={{color: 'orange'}}></i>
+                                    <span>({book[0].volumeInfo.averageRating} /5)</span> 
+                                     
+                                </div>) || 
+
+                                (Math.round(book[0].volumeInfo.averageRating) === 5 &&
+                                    <div className="evaluation">
+                                      Rating  :   
+                                    <i className="fa fa-star" style={{color: 'orange',marginLeft: '10px'}}></i>
+                                    <i className="fa fa-star" style={{color: 'orange'}} ></i>
+                                    <i className="fa fa-star" style={{color: 'orange'}}></i>
+                                    <i className="fa fa-star" style={{color: 'orange'}}></i>
+                                    <i className="fa fa-star" style={{color: 'orange'}}></i>
+                                        <span>({Math.round(book[0].volumeInfo.averageRating)} /5)</span> 
+                                         
+                                </div>))
+                                
+                                    
+                                : <span> No rating yet </span>
                                 }
 
-                                <button className="btn-shelf"> <i className="fa fa-plus"></i> Add To Shelf Book</button>
+                                <button className="btn-shelf" > <i className="fa fa-plus"></i> Add To Shelf Book</button>
                             </div>
                          
                             <ul className="list-unstyled details" style={{margin: '70px 0'}}>
-                                <li><span>Author</span>: {book.length > 0 && book[0].volumeInfo.authors[0]}</li>
+                                {book.length > 0 && book[0].volumeInfo.authors[0] ? 
+                                <li><span>Author</span>: {book.length > 0 && book[0].volumeInfo.authors[0]}</li> :
+                                <li><span>Author</span>: no information </li> }
                                 <li><span>Number Of Pages</span>: {book.length > 0 && book[0].volumeInfo.pageCount}</li>
                                 <li><span>Publication Year</span>: {book.length > 0 && book[0].volumeInfo.publishedDate}</li>
                             </ul>
@@ -89,7 +176,7 @@ const BookDetails = (props) => {
                     </div>
                     <div className="col-xs-12 col-sm-12 col-md-12 col-lg-3">
                         <div className="reviews">
-                            <h4>Popular Reviews (5)</h4>
+                            <h4>Popular Reviews </h4>
                             <div className="box-person">
                                 <div className="img">
                                 <i className="fa fa-user"></i>
@@ -123,13 +210,13 @@ const BookDetails = (props) => {
                                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                                 </div>
                             </div>
-                            <a href="#" className="more">See More..</a>
+                            {/* <a href="#" className="more">See More..</a> */}
                         </div>
                         <div className="replay">
                             <h4>Reviews (7)</h4>
                             <div className="box-person">
                                 <div className="img">
-                                
+                                <i className="fa fa-user"></i>
                                 </div>
                                 <div className="info-details">
                                     <h5>Ola Gamal</h5>
@@ -146,7 +233,7 @@ const BookDetails = (props) => {
                             </div>
                             <div className="box-person">
                                 <div className="img">
-                                
+                                <i className="fa fa-user"></i>
                                 </div>
                                 <div className="info-details">
                                     <h5>Ahmed Emara</h5>
@@ -162,15 +249,67 @@ const BookDetails = (props) => {
                                     <a href="#" className="report">Report</a>
                                 </div>
                             </div>
-                            <a href="#" className="more">See More..</a>
+                            {/* <a href="#" className="more">See More..</a> */}
+                            
                         </div>
+                        <input type="text" name="mail" placeholder="Add review" style={{width: '254px',marginBottom: '5px'}}/>
+                        
+                        <div className="evaluation">
+                        rate this book :
+                                    <i className="fa fa-star" style={{fontSize: '20px',color: 'gray',marginLeft: '10px'}}></i>
+                                    <i className="fa fa-star" style={{fontSize: '20px',color: 'gray'}}></i>
+                                    <i className="fa fa-star" style={{fontSize: '20px',color: 'gray'}}></i>
+                                    <i className="fa fa-star" style={{fontSize: '20px',color: 'gray'}}></i>
+                                    <i className="fa fa-star" style={{fontSize: '20px',color: 'gray',marginRight: '10px'}}></i>
+                                     0/5
+                                    </div>
                         <div className="mail">
-                            <h4>Invite Friends</h4>
-                            <p>I Strong Recommend This Book For You!</p>
-                            <form action="">
+                            <h4>Share with Friends</h4>
+                            {/* <p>I Strong Recommend This Book For You!</p> */}
+                            {/* <form action="">
                                 <input type="email" name="mail" placeholder="Your Friends" />
-                                <input type="submit" value="Go" />
-                            </form>
+                                <input type="submit" value="Go" /><i className="fa fa-massenger"></i>
+                            </form> */}
+                            {/* <a href="#"><i class="fab fa-facebook-messenger" style={{fontSize: '27px',color: '#f5b17b'}}></i></a>
+
+                            <a href="#"><i class="fa fa-google" aria-hidden="true" style={{fontSize: '30px',marginLeft: '10px',color: '#f5b17b'}}></i></a>
+                            <a href="#"><i class="fa fa-twitter" aria-hidden="true" style={{fontSize: '30px',marginLeft: '10px',color: '#f5b17b'}}></i></a> */}
+                            {/* <TwitterIcon size={30} round={true} style={{marginTop: '-10px',marginLeft: '10px'}} /> */}
+                            <EmailShareButton 
+                             url={window.location.href}
+                             body="I Strong Recommend This Book For You!" 
+                            >
+                            <EmailIcon size={30} logoFillColor="#f5b17b" round={true} style={{marginTop: '10px',marginLeft: '10px'}} /> </EmailShareButton>
+                            
+                            <TwitterShareButton 
+                             url={window.location.href}
+                             quote="I Strong Recommend This Book For You!" 
+                            >
+                            <TwitterIcon size={30} logoFillColor="#f5b17b" round={true} style={{marginTop: '10px',marginLeft: '10px'}} /> </TwitterShareButton>
+                            
+
+                            <FacebookShareButton 
+                             url={window.location.href}
+                             quote={"I Strong Recommend This Book For You!"}
+                             hashtag="#my favourite book"
+
+                            >
+                            <FacebookIcon size={30} logoFillColor="#f5b17b" round={true} style={{marginTop: '10px',marginLeft: '10px'}} /> </FacebookShareButton>
+                            
+                            
+                            <LinkedinShareButton
+                             url={window.location.href}
+                            >
+                            <LinkedinIcon size={30} logoFillColor="#f5b17b" round={true} style={{marginTop: '10px',marginLeft: '10px'}} /> </LinkedinShareButton>
+                            
+                            
+                            <WhatsappShareButton
+                            title="I Strong Recommend This Book For You!"
+                            url={window.location.href}
+                            >
+                           <WhatsappIcon size={30} logoFillColor="#f5b17b" round={true} style={{marginTop: '10px',marginLeft: '10px'}}/></WhatsappShareButton>
+                           
+                            
                         </div>
                     </div>
                 </div>
@@ -180,7 +319,8 @@ const BookDetails = (props) => {
     <div className="slider">
         <div className="container">
             <div className="info">
-                <span>Similar Books</span><span>(5 Books)</span>
+                <span>Similar Books</span>
+                {/* <span>(5 Books)</span> */}
             </div>
             <div className="up">
                 <div className="client active">
