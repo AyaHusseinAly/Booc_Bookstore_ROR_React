dd1366df7882c2591f3f72c005b7bc439ec790c8
aya hussein <aya.hussein3107@gmail.com>
2021-06-21 05:55:17 +0200

    community likes/comments static

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -15,8 +15,8 @@ class ChaptersController < ApplicationController
                 content: story.summary,
                 story_title:story.title,
                 chapter_title:"Summary",
-                liked_users:[1,2,3],
-                comments:[{user_id:1,comment_content:"nice story"},{user_id:2,comment_content:"Great story"}],
+                liked_users:[{user_name:'Fatma Tarek',user_img:"img/exPP.png", user_id: '1'},{user_name:'Mona Youssef',user_img:"img/exPP.png", user_id: '2'},{user_name:'Eman Hussein',user_img:"img/exPP.png", user_id: '3'},{user_name:'Amal Tamam',user_img:"img/exPP.png", user_id: '4'},{user_name:'Fatma Tarek',user_img:"img/exPP.png", user_id: '1'},{user_name:'Mona Youssef',user_img:"img/exPP.png", user_id: '2'},{user_name:'Eman Hussein',user_img:"img/exPP.png", user_id: '3'},{user_name:'Amal Tamam',user_img:"img/exPP.png", user_id: '4'}],
+                comments:[{user_id:1,user_name:'Fatma Tarek',user_img:"img/exPP.png",comment_content:"nice work!"},{user_id:2,user_name:'Fatma Tarek',user_img:"img/exPP.png", comment_content:"Great story"}],
                 created_at:story.created_at
 
             }
@@ -29,8 +29,8 @@ class ChaptersController < ApplicationController
                 content: chapter.summary,
                 story_title:chapter.short_story.title,
                 chapter_title:chapter.title,
-                liked_users:[1,2,3],
-                comments:[{user_id:1,comment_content:"nice story"},{user_id:2,comment_content:"Great story"}],
+                liked_users:[{user_name:'Fatma Tarek',user_img:"img/exPP.png", user_id: '1'},{user_name:'Mona Youssef',user_img:"img/exPP.png", user_id: '2'},{user_name:'Eman Hussein',user_img:"img/exPP.png", user_id: '3'},{user_name:'Amal Tamam',user_img:"img/exPP.png", user_id: '4'}],
+                comments:[{user_id:1,user_name:'Fatma Tarek',user_img:"img/exPP.png",comment_content:"nice work!"},{user_id:2,user_name:'Fatma Tarek',user_img:"img/exPP.png", comment_content:"Great story"},{user_id:1,user_name:'Fatma Tarek',user_img:"img/exPP.png",comment_content:"nice work!"},{user_id:2,user_name:'Fatma Tarek',user_img:"img/exPP.png", comment_content:"Great story"},{user_id:1,user_name:'Fatma Tarek',user_img:"img/exPP.png",comment_content:"nice work!"},{user_id:2,user_name:'Fatma Tarek',user_img:"img/exPP.png", comment_content:"Great story"},{user_id:1,user_name:'Fatma Tarek',user_img:"img/exPP.png",comment_content:"nice work!"},{user_id:2,user_name:'Fatma Tarek',user_img:"img/exPP.png", comment_content:"Great story"}],
                 created_at:chapter.created_at
 
             }
