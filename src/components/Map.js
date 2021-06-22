e4bf86a32ac20e9373aca11e75f79b41949f74a0
Fatma-Tarek <fatma.tarek204@gmail.com>
2021-06-22 18:15:37 +0200

    handle errors in share Location

--- src/components/Map.js
+++ src/components/Map.js
@@ -39,7 +39,7 @@ class Map extends Component {
     /******************** Share my Loction *****************************/
     getLocation = () => {
         if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(this.getCoordinates);
+          navigator.geolocation.getCurrentPosition(this.getCoordinates, this.handleLocationError);
         } else {
            alert("Geolocation is not supported by this browser.");
         }
@@ -53,15 +53,34 @@ class Map extends Component {
             longitudeOfMyPosition:position.coords.longitude,
         })
     }
+
+    handleLocationError = (error) => {
+        switch(error.code) {
+            case error.PERMISSION_DENIED:
+               alert("User denied the request for Share my.");
+              break;
+            case error.POSITION_UNAVAILABLE:
+                alert("Location information is unavailable.");
+              break;
+            case error.TIMEOUT:
+                alert("The request to get user location timed out.");
+              break;
+            case error.UNKNOWN_ERROR:
+                alert("An unknown error occurred.");
+              break;
+            default: 
+                alert("An unknown error occurred.");
+        }
+    }
     /******************** End  Share my Loction *****************************/
 
+    /******************** Submit The Form *****************************/
     onValueChange= (event) => {
         this.setState({
           selectedOption: event.target.value
         });
     } 
 
-    /******************** Submit The Form *****************************/
     handleSubmit = async e => {
         e.preventDefault();
         const data = new FormData(e.target);
@@ -152,7 +171,7 @@ class Map extends Component {
                 {this.state.stores.map((marker, index) => (
                     <Marker
                     key={index}
-                    {...marker}
+                    //{...marker}
                     // draggable={true}
                     // onDragEnd={this.onMarkerDragEnd}
                     position={marker.position}
