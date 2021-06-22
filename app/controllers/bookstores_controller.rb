68407806521fe07fbf46013b274c64ed4eddf522
Fatma-Tarek <fatma.tarek204@gmail.com>
2021-06-22 15:07:34 +0200

    search by kind only

--- app/controllers/bookstores_controller.rb
+++ app/controllers/bookstores_controller.rb
@@ -11,9 +11,31 @@ class BookstoresController < ApplicationController
   
       end
 
+    def usable
+    @poistion = []
+        @stores = []
+        @Bookstore.each do |bookstore|
+            positionobj={
+                lat: bookstore.lat,
+                lng: bookstore.lng,
+            }
+            stores= {
+                    id: bookstore.id,
+                    name:bookstore.name,
+                    phone: bookstore.phone, 
+                    kind: bookstore.kind, 
+                    img: bookstore.img,
+                    created_at: bookstore.created_at,
+                    updated_at: bookstore.updated_at, 
+                    position: positionobj,
+            }
+            @stores.push(stores);
+        end
+        render :json =>{stores: @stores}
+    end
+
     def search
         if params['bookName'] != "" && params['selectedOption'] == nil
-            #@Bookstore = Bookstore.where("name LIKE ?","%"+ params['bookName']+"%").where(kind: params['selectedOption'])
             @Bookstore = Bookstore.where("name LIKE ?","%"+ params['bookName']+"%")
             @poistion = []
             @stores = []
@@ -58,9 +80,32 @@ class BookstoresController < ApplicationController
                 @stores.push(stores);
             end
             render :json =>{stores: @stores}
+        elsif params['bookName'] == "" && params['selectedOption'] != nil
+            @Bookstore = Bookstore.where(kind: params['selectedOption'])
+            @poistion = []
+            @stores = []
+            @Bookstore.each do |bookstore|
+                positionobj={
+                    lat: bookstore.lat,
+                    lng: bookstore.lng,
+                }
+                stores= {
+                    id: bookstore.id,
+                    name:bookstore.name,
+                    phone: bookstore.phone, 
+                    kind: bookstore.kind, 
+                    img: bookstore.img,
+                    created_at: bookstore.created_at,
+                    updated_at: bookstore.updated_at, 
+                    position: positionobj,
+                }
+                @stores.push(stores);
+            end
+            render :json =>{stores: @stores}
         else
            render :json =>{stores: @stores}
            #render :json => {message:"hello from back", title:params['bookName'],kind:params['selectedOption'],distinct:params[ "distict"],sharemyLocation:params["sharemyLocation"]}
         end
     end
+
 end
