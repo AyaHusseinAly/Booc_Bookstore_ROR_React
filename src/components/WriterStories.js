4ea62ece3b9a9b88030a932db7bd39bdee84abdd
shrook <shrooktageldin@gmail.com>
2021-06-26 12:07:14 +0200

    bookmark list , style for writerStories

--- src/components/WriterStories.js
+++ src/components/WriterStories.js
@@ -9,6 +9,8 @@ import "slick-carousel/slick/slick-theme.css";
 import '../style/slider.css';
 import Slider from "react-slick";
 import Rating from './Rating';
+import { Link } from "react-router-dom";
+
 
 
 
@@ -63,15 +65,7 @@ class WriterStories extends Component {
         return (
 
             <div >
-                {/* {this.state.shortstoiesNotFinished.length + this.state.shortstoiesFinished.length == 0 && <div className='mt-5 pt-5' style={{ width: '100%', height: '100%' }}>
-                    <h3 className='d-flex justify-content-center'>You haven't add any stories </h3>
-                    <div className='d-flex justify-content-center'> <button className="py-2 px-5  my-3 ml-3 btn btn-lg" style={{ backgroundColor: '#F8A488', borderColor: '#F8A488' }} ><i className="fa fa-plus-circle" style={{ color: 'black', border: 'none' }} onClick={() => this.props.history.push('/addstory')}>Add New Story</i></button></div>
-                </div>} */}
                 <div className='ml-5'>
-                    {/* <div className='d-flex justify-content-end mr-2'>
-
-                        <button className="py-1 px-3  my-4 mr-4 btn btn-lg" style={{ backgroundColor: '#F8A488', borderColor: '#F8A488' }} ><i className="fa fa-plus-circle" style={{ color: 'black', border: 'none' }} onClick={() => this.props.history.push('/addstory')}>Add New Story</i></button>
-                    </div> */}
                     <div className="info-person">
                         <div className="container">
                             <div className="content-info">
@@ -90,13 +84,12 @@ class WriterStories extends Component {
                                 <div className="active" >Writer Previous Work</div>
 
                             </div>
-                            {/* {this.state.shortstoiesNotFinished.length + this.state.shortstoiesFinished.length > 0 && */}
 
                             <div className="container light" style={{ border: '2px #ADB4C3 solid' }}>
                                 <div >
                                     <div >
                                         <div className="">
-                                            <div className="" style={{ marginBottom: "120px" }}>
+                                            <div className="" style={{ marginBottom: "130px" }}>
                                                 <h3>Fininshed Stories</h3>
                                                 {this.state.shortstoiesFinished && this.state.shortstoiesFinished.length > 0 && <div className="img">
                                                     <div className="row" style={{ width: "1300px", marginTop: "-120px" }}>
@@ -121,19 +114,8 @@ class WriterStories extends Component {
 
                                             </div>
 
-                                            {/* <div className="" style={{}}>
-                                                <h3>Book Marks</h3>
-                                                {this.state.bookmarks && this.state.bookmarks.length > 0 && <div className="img">
-                                                    <div className="row" style={{ width: "1300px", marginTop: "-120px" }}>
-                                                        <Favr items={this.state.bookmarks} style={{ height: "250px", marginTop: '-130px' }} />
-
-                                                    </div>
-                                                </div>}
-                                                {this.state.bookmarks && this.state.bookmarks.length == 0 && <div className="section-select text-center">You Didn't bookmark any stories for this writer</div>}
-
-                                            </div> */}
                                             <div className="" style={{ marginTop: "150px" }}>
-                                                <h3 style={{}}>Book Marks</h3>
+                                                {/* <h3 style={{}}>Book Marks</h3> */}
                                                 <div className="img">
                                                     <div className="row" style={{ width: "1300px" }}>
 
@@ -164,15 +146,15 @@ class WriterStories extends Component {
 function Favr(props) {
     const renderSlides = () =>
         props.items.map(story => (
-            <div>
-                <div>{<img src={story.cover} style={{ width: "150px", height: "200px", margin: "auto" }} />}</div>
+            <Link to={`/shortStory/${story.id}`}>
+                <div >{<img src={story.cover} style={{ width: "150px", height: "200px", margin: "auto" }} />}</div>
                 <figcaption style={{ textAlign: "center", fontSize: "15px", color: "var(--primaryColor)" }}><p className="mb-0 pb-0">{story.title}</p><p className="mt-0 pt-0"><Rating rate={story.rate} /></p></figcaption>
 
-            </div>
+            </Link>
         ));
 
     return (
-        <div className="Appi">
+        <div className="Appi" style={{ width: '850px', marginLeft: '-300px' }}>
 
             {props.items.length != 0 && props.items.length <= 5 && <Slider dots={true} infinite={true} slidesToShow={props.items.length} slidesToScroll={1}>
                 {renderSlides()}
